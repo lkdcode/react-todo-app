d15d27723b406e3e08ad4524fd4c5be218f8329f
lkdcode <dlrlejr1@naver.com>
2023-05-23 11:26:23 +0900

    useEffect

--- react-basic/src/component/chap03_state/Counter.js
+++ react-basic/src/component/chap03_state/Counter.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Counter = () => {
 
   // let number = 0;
 
   const [number, setNumber] = useState(0);
+  const [nick, setNick] = useState('익명');
 
   const increase = () => { setNumber(number + 1); };
   const decrease = () => { setNumber(number - 1); };
 
+  const changeNick = () => { setNick('김철수'); };
+
+  
+
+  useEffect(() => {
+    // 화면이 처음 렌더링될 때 서버 api 호출하는 경우
+    // 처음 1번만 호출하고 또 할 필요가 있을까??
+    console.log('3. useEffect Call !!');
+    console.log(`4. nick: ${nick}, number: ${number}`);
+  }, [nick]);
+
+
+  
+  console.log('1. component function execute!');
+
+
+
   return (
     <>
-      <h1>{number}</h1>
+      {console.log('2. redered html !!')}
+      <h1>닉네임 : {nick}</h1>
+      <h2>{number}</h2>
       <button onClick={increase}>+1</button>
       <button onClick={decrease}>-1</button>
+      <button onClick={changeNick}>변경</button>
     </>
   )
 }
