602bdb2f0b9e473813aea866386c9071460df72c
lkdcode <dlrlejr1@naver.com>
2023-05-24 09:25:39 +0900

    initial commit

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoTemplate from './component/TodoTemplate';
+
 
 function App() {
   return (
-    <h1>hello world!</h1>
+   <TodoTemplate />
   );
 }
 
